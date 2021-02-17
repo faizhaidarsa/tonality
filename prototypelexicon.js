868145a4d835dfcf7d09450299fa727141aa6ee5
unknown <faizhaidarsa@gmail.com>
2021-02-17 10:48:55 +0700

    penyempurnaan

--- prototypelexicon.js
+++ prototypelexicon.js
@@ -1,33 +1,3 @@
-var sj = `Demokrat menduga Presiden Joko Widodo (Jokowi) sedang menyiapkan putranya, Gibran Rakabuming Raka maju Pilgub DKI Jakarta 2024, di balik penghentian pembahasan RUU Pemilu.
-
-DPP PDI Perjuangan (PDIP) menilai, pemikiran tersebut sangat pragmatis.
-
-"Kok pola pikirnya sangat pragmatis dan dangkal ya," kata Ketua DPP PDIP Djarot Saiful Hidayat saat dihubungi Tribunnews.com, Kamis (11/2/2021).
-
-
-"Bukankah kepentingan nasional yang harus lebih didahulukan kesehatan, pemulihan ekonomi rakyat," imbuhnya.
-
-Menurut Djarot, dibutuhkan konsistensi dalam penerapan sebuah Undang-Undang.
-
-Di mana dalam Undang-undang Nomor 10 Tahun 2016 mengatur penyelenggaraan Pilkada 2024.
-
-"Konsistensi dalam menjalankan Undang-undang pilkada yang sudah disepakati di tahun 2024," ujarnya.
-
-Baca juga: Demokrat Ngotot Pilkada 2022, Gerindra: Mungkin AHY Mau Maju di Pilgub DKI
-
-Baca juga: Arief: Hanya Gibran yang Bisa Saingi Anies di Pilkada DKI Mendatang
-
-Baca juga:  Viral Masjid Hanyut Sampai ke Laut, Alat Salat dan Pengeras Suara Ikut Terbawa
-
-Terkait Pilkada 2024, Djarot menyatakan PDIP terus merapatkan barisan dan menyiapkan kader-kader terbaiknya.
-
-"Tentang Pilkada di tahun 2024 tentunya partai ada mekanismenya tersendiri dan kita terus melalukan konsolidasi partai dan menyiapkan kader-kader pemimpin yang baik dan tetap teguh menjalankan Ideologi Pancasila," pungkas Djarot.
-
-Demokrat menduga Presiden Joko Widodo (Jokowi) sedang menyiapkan putranya, Gibran Rakabuming Raka maju Pilgub DKI Jakarta 2024, di balik penghentian pembahasan RUU Pemilu.
-
-DPP PDI Perjuangan (PDIP) menilai, pemikiran tersebut sangat pragmatis.
-
-"Kok pola pikirnya sangat pragmatis dan dangkal ya," kata Ketua DPP PDIP Djarot Saiful Hidayat saat dihubungi Tribunnews.com, Kamis (11/2/2021).`;
 
 //Menghilangkan tanda baca spasi
 function hapusKarakter(kalimat){
@@ -54,7 +24,7 @@ function hitungBobot(listToken){
     var tokenBobot = [s,0]
     for (i = 0;i<lexicon.length;i++){
       if(s.toLowerCase()==lexicon[i].word.toLowerCase()){
-        tokenBobot[1] = parseInt(lexicon[i].weight)
+        tokenBobot[1] += parseInt(lexicon[i].weight)
       }
     }
     return tokenBobot
